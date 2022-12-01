2a0742436f269912b304269c55d9a71022a0f387
anatoliy-avramenko <anatollko91@gmail.com>
2022-12-01 13:40:54 +0100

    Day 1

--- src/day1.test.js
+++ src/day1.test.js
@@ -2,10 +2,39 @@ import fs from 'fs';
 
 const part1 = () => {
   const input = fs.readFileSync('src/day1-input.txt')
-    .toString();
-  console.log(input);
-}
+    .toString()
+    .split('\n\n')
+    .map(el => el
+      .split('\n')
+      .map(el => Number(el))
+    );
 
-it('should ', () => {
-  expect(part1()).toBeTruthy();
-});
+  let max = [];
+  input.forEach(elf => {
+    const sum = elf.reduce((acc, next) => acc + next, 0);
+    if (sum > max) {
+      max = sum;
+    }
+  });
+
+  return max;
+}
+const part2 = () => {
+  const input = fs.readFileSync('src/day1-input.txt')
+    .toString()
+    .split('\n\n')
+    .map(el => el
+      .split('\n')
+      .map(el => Number(el))
+    );
+
+  let maxArr = [];
+  input.forEach(elf => {
+    const sum = elf.reduce((acc, next) => acc + next, 0);
+    maxArr.push(sum);
+  });
+
+  return maxArr.sort((a,b,) => a - b)
+    .slice(-3)
+    .reduce((a, b) => +a + +b, 0);
+};
