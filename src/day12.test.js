0b98b64a52dabff23a0678b52522f0b3bf3fac96
anatoliy-avramenko <anatollko91@gmail.com>
2023-01-12 22:37:28 +0100

    preliminary day 12

--- src/day12.test.js
+++ src/day12.test.js
@@ -1,7 +1,23 @@
 import fs from 'fs';
 
+const findInMatrix = (el) => {
+  let targetIndex;
+  Object.entries(directory).find(([index, val]) => {
+    if (val === el) {
+      targetIndex = index;
+    }
+    return val === el;
+  })
+  return targetIndex;
+};
+const parseCoords = (coords) => {
+  return coords.split('-').map(el => Number(el));
+}
+
 const directory = {};
-const rows = fs.readFileSync('src/day12-input.txt')
+const open = [];
+const closed = [];
+fs.readFileSync('src/day12-input.txt')
   .toString()
   .split('\n')
   .map((row, y) => row
@@ -14,92 +30,31 @@ const rows = fs.readFileSync('src/day12-input.txt')
       return el;
     })
   );
-const visited = {};
-const yLength = rows.length;
-const xLength = rows[0].length;
-
-const findInMatrix = (el) => {
-  let targetIndex;
-  Object.entries(directory).find(([index, val]) => {
-    if (val === el) {
-      targetIndex = index;
-    }
-    return val === el;
-  })
-  return targetIndex;
-};
 const start = findInMatrix(0);
+const [startY, startX] = parseCoords(start);
 const end = findInMatrix(27);
+const [endY, endX] = parseCoords(end);
 
-const parseCoords = (coords) => {
-  return coords.split('-').map(el => parseInt(el));
-};
+class Node {
+  constructor(coords, value) {
+    this.coordsStr = coords;
+    const [y, x] = parseCoords(coords);
+    this.y = y;
+    this.x = x;
+    this.value = directory[coords];
+    this.parentNode = null;
+    this.totalCost = 0;
+    this.toStart = 0;
+    this.toEnd = 0;
+  }
+}
 
-const getNeighbours = (coords, val) => {
-  // should be neighbour
-  // should be valid elevation
-  // should be directed towards destination
-  const lastYIndex = yLength - 1;
-  const lastXIndex = xLength - 1;
-  const neighbours = {};
-  const [y, x] = parseCoords(coords);
-  const upY = (y !== 0 ? y - 1 : null);
-  const up = Number.isInteger(upY) ? `${upY}-${x}` : null;
 
-  const downY = (y !== lastYIndex ? y + 1 : null);
-  const down = Number.isInteger(downY) ? `${downY}-${x}` : null;
 
-  const leftX = (x !== 0 ? x - 1 : null);
-  const left = Number.isInteger(leftX) ? `${y}-${leftX}` : null;
-
-  const rightX = (x !== lastXIndex ? x + 1 : null);
-  const right = Number.isInteger(rightX) ? `${y}-${rightX}` : null;
-  const availableNeighbours = [up, down, left, right]
-    .filter(Boolean)
-    .filter(neighbour => {
-      return !visited[neighbour] && Math.abs(directory[neighbour] - val) < 2;
-    })
-    .reduce((acc, neigh) => {
-      const [endY, endX] = parseCoords(end);
-      const [neighY, neighX] = parseCoords(neigh);
-      let score = Math.abs(endY - neighY) + Math.abs(endX - neighX);
-      const isIncrease = (directory[neigh] - val) === 1;
-      const isEnd = directory[neigh] === 'E';
-      acc.push({
-        coords: neigh,
-        val: directory[neigh],
-        score: isEnd ? Infinity : (isIncrease ? score - 1 : score),
-      });
-      return acc;
-    }, [])
-    .sort((a, b) => a.score - b.score);
-
-  return availableNeighbours;
-};
 
 const part1 = () => {
-  let stepsCounter = 0;
-  let currentCoords = start;
-  let currentVal = directory[currentCoords];
-  visited[currentCoords] = true;
-  const makeStep = () => {
-    const potentialSteps = getNeighbours(currentCoords, currentVal);
-    currentCoords = (potentialSteps[0]).coords;
-    currentVal = directory[currentCoords];
-    visited[(potentialSteps[0]).coords] = true;
-
-    console.log();
-  };
-
-  while (true) {
-    makeStep();
-    stepsCounter += 1;
-    if (currentCoords === end) {
-      break;
-    }
-  }
-  return stepsCounter;
-  console.log(rows);
+  const startingNode = new Node(start);
+  console.log();
 };
 
 
